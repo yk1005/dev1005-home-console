e7ca3ec880f40b8c6e5efb06be41d5dfff563561
yk1005 <dev.yk1005@gmail.com>
2024-11-22 23:55:00 +0900

    余計なimport削除

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 'use client';
 import { createTheme } from '@mui/material/styles';
 import { Roboto } from 'next/font/google';
-import { blueGrey, grey } from '@mui/material/colors'
+import { blueGrey } from '@mui/material/colors'
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
