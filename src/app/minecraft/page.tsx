52da270903d2b7d9ae49496b58fd21ae044479f5
yk1005 <dev.yk1005@gmail.com>
2024-11-23 00:11:47 +0900

    Minecraftページ タブ化

--- src/app/minecraft/page.tsx
+++ src/app/minecraft/page.tsx
@@ -1,70 +1,22 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { Box, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography, Container, Button, CircularProgress } from '@mui/material';
-import RefreshIcon from '@mui/icons-material/Refresh';
+import { Container, Toolbar, Tab, Tabs } from '@mui/material';
+import OnlineUsersView from './views/OnlineUsers';
 
-import { fetchOnlineUsers, User } from '@/actions/rcon';
 
 export default function Minecraft() {
-    const [isLoading, setIsLoading] = useState<boolean>();
-    const [users, setUsers] = useState<Array<User>>();
-
-    const refresh = () => {
-        setIsLoading(true)
-
-        fetchOnlineUsers()
-            .then((users) => setUsers(users))
-            .finally(() => { setIsLoading(false) })
-    }
-
-    useEffect(() => {
-        refresh()
-    }, [])
+    const [tab, setTab] = useState<number>(0);
 
     return (
         <Container maxWidth="lg" sx={{ paddingY: "16px" }}>
-            <Box display={"flex"}>
-                <Typography variant='h4' flexGrow={1}>Online Users</Typography>
-                <Box alignContent={"center"}>
-                    <Button variant='outlined' onClick={() => refresh()}>
-                        <RefreshIcon />
-                    </Button>
-                </Box>
-            </Box>
-            <Paper sx={{ marginY: "16px" }}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell sx={{ width: "30%" }}>Name</TableCell>
-                            <TableCell sx={{ width: "70%" }}>UUID</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {isLoading && (
-                            <TableRow>
-                                <TableCell colSpan={2} align='center'>
-                                    <CircularProgress />
-                                </TableCell>
-                            </TableRow>
-                        )}
+            <Tabs value={tab} onChange={(_, val) => { setTab(val) }}>
+                <Tab label="Online Users" value={0}/>
+            </Tabs>
+            <Toolbar />
 
-                        {!isLoading && (
-                            users?.length ? users.map((user) => (
-                                <TableRow key={user.uuid}>
-                                    <TableCell>{user.name}</TableCell>
-                                    <TableCell>{user.uuid}</TableCell>
-                                </TableRow>
-                            )) :
-                                <TableRow>
-                                    <TableCell colSpan={2}>
-                                        <Typography>No online users.</Typography>
-                                    </TableCell>
-                                </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </Paper>
+            {tab == 0 && <OnlineUsersView />}
+
         </Container>
     );
 }
---
+++ src/app/minecraft/views/OnlineUsers.tsx
@@ -0,0 +1,70 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+import { Box, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography, Button, CircularProgress } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/Refresh';
+
+import { fetchOnlineUsers, User } from '@/actions/rcon';
+
+export default function OnlineUsersView() {
+    const [isLoading, setIsLoading] = useState<boolean>();
+    const [users, setUsers] = useState<Array<User>>();
+
+    const refresh = () => {
+        setIsLoading(true)
+
+        fetchOnlineUsers()
+            .then((users) => setUsers(users))
+            .finally(() => { setIsLoading(false) })
+    }
+
+    useEffect(() => {
+        refresh()
+    }, [])
+
+    return (
+        <>
+            <Box display={"flex"}>
+                <Typography variant='h4' flexGrow={1}>Online Users</Typography>
+                <Box alignContent={"center"}>
+                    <Button variant='outlined' onClick={() => refresh()}>
+                        <RefreshIcon />
+                    </Button>
+                </Box>
+            </Box>
+            <Paper sx={{ marginY: "16px" }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ width: "30%" }}>Name</TableCell>
+                            <TableCell sx={{ width: "70%" }}>UUID</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {isLoading && (
+                            <TableRow>
+                                <TableCell colSpan={2} align='center'>
+                                    <CircularProgress />
+                                </TableCell>
+                            </TableRow>
+                        )}
+
+                        {!isLoading && (
+                            users?.length ? users.map((user) => (
+                                <TableRow key={user.uuid}>
+                                    <TableCell>{user.name}</TableCell>
+                                    <TableCell>{user.uuid}</TableCell>
+                                </TableRow>
+                            )) :
+                                <TableRow>
+                                    <TableCell colSpan={2}>
+                                        <Typography>No online users.</Typography>
+                                    </TableCell>
+                                </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </Paper>
+        </>
+    );
+}
